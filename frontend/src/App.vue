0250546be16843e7e9f5696478f7aac4c5578149
DanTe <cldroddl@naver.com>
2020-04-29 01:24:55 +0900

    add GoogleKeep

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,76 +1,70 @@
 <template>
-  <v-app id="inspire">
+  <v-app id="keep">
+    <v-app-bar
+      app
+      clipped-left
+      color="amber"
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <span class="title ml-3 mr-5">Google&nbsp;<span class="font-weight-light">Keep</span></span>
+      <v-text-field
+        solo-inverted
+        flat
+        hide-details
+        label="Search"
+        prepend-inner-icon="search"
+      />
+
+      <v-spacer />
+    </v-app-bar>
+
     <v-navigation-drawer
       v-model="drawer"
-      :clipped="$vuetify.breakpoint.lgAndUp"
       app
+      clipped
+      color="grey lighten-4"
     >
-      <v-list dense>
-        <template v-for="item in items">
-          <v-layout
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template v-for="(item, i) in items">
+          <v-row
             v-if="item.heading"
-            :key="item.heading"
-            row
-            align-center
+            :key="i"
+            align="center"
           >
-            <v-flex xs6>
+            <v-col cols="6">
               <v-subheader v-if="item.heading">
                 {{ item.heading }}
               </v-subheader>
-            </v-flex>
-            <v-flex
-              xs6
-              class="text-xs-center"
+            </v-col>
+            <v-col
+              cols="6"
+              class="text-right"
             >
-              <a
-                href="#!"
-                class="body-2 black--text"
-              >EDIT</a>
-            </v-flex>
-          </v-layout>
-          <v-list-group
-            v-else-if="item.children"
-            :key="item.text"
-            v-model="item.model"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon=""
-          >
-            <template v-slot:activator>
-              <v-list-item>
-                <v-list-item-content>
-                  <v-list-item-title>
-                    {{ item.text }}
-                  </v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </template>
-            <v-list-item
-              v-for="(child, i) in item.children"
-              :key="i"
-              @click="''"
-            >
-            <!-- link 대신 @click="''" 사용 가능 -->
-              <v-list-item-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-item-action>
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ child.text }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-group>
+              <v-btn
+                small
+                text
+              >edit</v-btn>
+            </v-col>
+          </v-row>
+          <v-divider
+            v-else-if="item.divider"
+            :key="i"
+            dark
+            class="my-4"
+          />
           <v-list-item
             v-else
-            :key="item.text"
+            :key="i"
             link
           >
-          <!-- link 대신 @click="''" 사용 가능 -->
             <v-list-item-action>
               <v-icon>{{ item.icon }}</v-icon>
             </v-list-item-action>
             <v-list-item-content>
-              <v-list-item-title>
+              <v-list-item-title class="grey--text">
                 {{ item.text }}
               </v-list-item-title>
             </v-list-item-content>
@@ -79,188 +73,31 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      app
-      color="blue darken-3"
-      dark
-    >
-      <v-toolbar-title
-        style="width: 300px"
-        class="ml-0 pl-3"
-      >
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <span class="hidden-sm-and-down">Google Contacts</span>
-      </v-toolbar-title>
-      <v-text-field
-        flat
-        solo-inverted
-        hide-details
-        prepend-inner-icon="search"
-        label="Search"
-        class="hidden-sm-and-down"
-      ></v-text-field>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>apps</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>notifications</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        large
-      >
-        <v-avatar
-          size="32px"
-          item
-        >
-          <v-img
-            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
-            alt="Vuetify"
-          >
-          </v-img></v-avatar>
-      </v-btn>
-    </v-app-bar>
     <v-content>
       <router-view/>
     </v-content>
-    <v-btn
-      bottom
-      color="pink"
-      dark
-      fab
-      fixed
-      right
-      @click="dialog = !dialog"
-    >
-      <v-icon>add</v-icon>
-    </v-btn>
-    <v-dialog
-      v-model="dialog"
-      width="800px"
-    >
-      <v-card>
-        <v-card-title class="grey darken-2">
-          Create contact
-        </v-card-title>
-        <v-container grid-list-sm>
-          <v-layout
-            row
-            wrap
-          >
-            <v-flex
-              xs12
-              align-center
-              justify-space-between
-            >
-              <v-layout align-center>
-                <v-avatar
-                  size="40px"
-                  class="mr-3"
-                >
-                  <img
-                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                    alt=""
-                  >
-                </v-avatar>
-                <v-text-field
-                  placeholder="Name"
-                ></v-text-field>
-              </v-layout>
-            </v-flex>
-            <v-flex xs6>
-              <v-text-field
-                prepend-icon="business"
-                placeholder="Company"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs6>
-              <v-text-field
-                placeholder="Job title"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field
-                prepend-icon="mail"
-                placeholder="Email"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field
-                type="tel"
-                prepend-icon="phone"
-                placeholder="(000) 000 - 0000"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field
-                prepend-icon="notes"
-                placeholder="Notes"
-              ></v-text-field>
-            </v-flex>
-          </v-layout>
-        </v-container>
-        <v-card-actions>
-          <v-btn
-            text
-            color="primary"
-          >More</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            color="primary"
-            @click="dialog = false"
-          >Cancel</v-btn>
-          <v-btn
-            text
-            @click="dialog = false"
-          >Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </v-app>
 </template>
 
 <script>
 export default {
   data: () => ({
-    dialog: false,
     drawer: null,
     items: [
-      { icon: 'home', text: 'Home', to: { name: 'home' } },
-      { icon: 'add_box', text: 'Test text', heading: 'Test Heading' },
-      { icon: 'contacts', text: 'Contacts' },
-      { icon: 'history', text: 'Frequently contacted' },
-      { icon: 'content_copy', text: 'Duplicates' },
-      {
-        icon: 'keyboard_arrow_up',
-        'icon-alt': 'keyboard_arrow_down',
-        text: 'Labels',
-        model: true,
-        children: [
-          { icon: 'add', text: 'Create label' }
-        ]
-      },
-      {
-        icon: 'keyboard_arrow_up',
-        'icon-alt': 'keyboard_arrow_down',
-        text: 'More',
-        model: false,
-        children: [
-          { text: 'Import' },
-          { text: 'Export' },
-          { text: 'Print' },
-          { text: 'Undo changes' },
-          { text: 'Other contacts' }
-        ]
-      },
+      { icon: 'lightbulb_outline', text: 'Notes' },
+      { icon: 'touch_app', text: 'Reminders' },
+      { divider: true },
+      { heading: 'Labels' },
+      { icon: 'add', text: 'Create new label' },
+      { divider: true },
+      { icon: 'archive', text: 'Archive' },
+      { icon: 'delete', text: 'Trash' },
+      { divider: true },
       { icon: 'settings', text: 'Settings' },
-      { icon: 'chat_bubble', text: 'Send feedback' },
+      { icon: 'chat_bubble', text: 'Trash' },
       { icon: 'help', text: 'Help' },
       { icon: 'phonelink', text: 'App downloads' },
-      { icon: 'keyboard', text: 'Go to the old version' },
-      { icon: 'help_outline', text: 'About', to: { name: 'about' } }
+      { icon: 'keyboard', text: 'Keyboard shortcuts' }
     ]
   })
 }
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,43 +1,42 @@
 <template>
   <v-container
     fluid
-    fill-height
+    class="grey lighten-4 fill-height"
   >
-    <v-layout
-      align-center
-      justify-center
+    <v-row
+      justify="center"
+      align="center"
     >
-      <v-tooltip left>
-        <template v-slot:activator="{ on }">
-          <v-btn
-            :href="'/'"
-            icon
-            large
-            v-on="on"
-          >
-          <!-- href 대신 :to를 사용해서 하면 잘 되는데 아이콘 옆에 그림자가 생긴다. -->
-          <!-- :to="{name : 'home'}" -->
-          <!-- 하지만 href는 객체를 인식하지 못한다. -->
-          <!-- :href="{name: 'home'}"  X  -->
-            <v-icon large>home</v-icon>
-          </v-btn>
-        </template>
-        <span>Home</span>
-      </v-tooltip>
-      <v-tooltip right>
-        <template v-slot:activator="{ on }">
-          <v-btn
-            icon
-            large
-            :href="'/about'"
-            v-on="on"
-          >
-            <v-icon large>help</v-icon>
-          </v-btn>
-        </template>
-        <span>About</span>
-      </v-tooltip>
-    </v-layout>
+      <v-col class="shrink">
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              :to="'/'"
+              icon
+              large
+              v-on="on"
+            >
+              <v-icon large>home</v-icon>
+            </v-btn>
+          </template>
+          <span>Home</span>
+        </v-tooltip>
+
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              :to="'/about'"
+              icon
+              large
+              v-on="on"
+            >
+              <v-icon large>help</v-icon>
+            </v-btn>
+          </template>
+          <span>About</span>
+        </v-tooltip>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
