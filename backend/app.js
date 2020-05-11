abbed64f1da0cc3ed62a9d3759feddee3cb136d4
DanTe <cldroddl@naver.com>
2020-05-12 00:24:42 +0900

    backend에서의 404처리, frontend에서 yarn build가 먼저 수행되어야 함.

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 
+const history = require('connect-history-api-fallback')
+
 var app = express();
 
 // view engine setup
@@ -21,12 +23,14 @@ app.use(cookieParser());
 
 app.use('/api', apiRouter);
 
+// app.use('/', indexRouter); // frontend의 / 가 나오도록 주석 처리
+app.use('/users', usersRouter);
+app.use(history())  // 잘못된 url을 모두 frontend로 보내 주기 위해 history 모드 사용
+
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+// 현재 404에러는 frontend에서 처리 하므로 다른 에러 처리를 위해서 사용한다.
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
