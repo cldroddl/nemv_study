0c0465b7c343d8ec9aed6b81c802f27d9af3dd1c
DanTe <cldroddl@naver.com>
2021-01-04 01:30:22 +0900

    corejs 3

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+    {
+      'uesBuiltIns': 'usage',
+      'corejs': 3,
+      'shippedProposals': true
+    }
   ]
 }
