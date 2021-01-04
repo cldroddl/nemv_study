5047f8ba89ed2e848e25d3d91788054439b40f6a
DanTe <cldroddl@naver.com>
2021-01-04 21:11:11 +0900

    backend 에 apiRouter 추가

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const apiRouter = require('./routes/api')
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +18,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// static 위에 정의해 준다.
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
 app.use(express.static(path.join(__dirname, 'public')));
 // frontend의 static 리소스를 사용할 경우 
 // frontend에서 outputDir을 backend의 public로 지정해 줘도 되고
@@ -24,9 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static(path.join(__dirname, 'frontend', 'dist')))
 // 여기서는 제목 설정등 여러가지 설정이 frontend에서 하는 것이 편할 것 같아
 // frontend에서 backend의 public 폴더로 복사해 주는 방식을 택한다.
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
