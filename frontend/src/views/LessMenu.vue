62f9eaca915bf1b2ebc0c69fe6486989fd7cbfcf
DanTe <cldroddl@naver.com>
2021-01-09 15:54:02 +0900

    less 1단 메뉴 완성

--- frontend/src/views/LessMenu.vue
+++ frontend/src/views/LessMenu.vue
@@ -47,7 +47,22 @@ nav {
   border-radius: 5px;
   height: 40px;
   ul {
-    list-style: none;
+    list-style: none; // none 으로 li 태그의 블릿 제거
+    li {
+      // 수평형 메뉴 만드는 규칙
+      display: inline;  // inline 으로 아래로 나오는 li 들을 옆으로 나오게
+      a {
+        display: inline-block;
+        float: left;
+        text-decoration: none;  // 밑줄 제거
+        line-height: 40px;    // nav 의 높이를 사용해서 텍스트가 중앙에 정렬 되도록...
+        padding: 0 20px;      // 좌우 패딩을 2필셀씩 적용해서 다른 텍스트와 일정한 간격 유지
+        color: @textColor;
+        &:hover { // & 는 부모 요소를 의미
+          color: @hoverColor;
+        }
+      }
+    }
   }
 }
 
