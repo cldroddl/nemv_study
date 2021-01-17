da8990e580d3622249a5c7172545e54d48439163
DanTe <cldroddl@naver.com>
2021-01-18 00:30:42 +0900

    Flex.vue 에 css 파일 import 해 봄

--- frontend/src/views/Flex.vue
+++ frontend/src/views/Flex.vue
@@ -27,6 +27,7 @@ export default {
 <style lang="scss" scoped>
 // less 는 @ 를 붙여 변수를 선언하고
 // sass 는 $ 를 붙여 변수를 선언한다.
+@import "~stylesPath/css/reset.css";
 $background: #ff6b57;
 
 * {margin:0; padding:0;}
--- frontend/src/views/LessMenu.vue
+++ frontend/src/views/LessMenu.vue
@@ -80,7 +80,7 @@ export default {
 @import url("http://fonts.googleapis.com/earlyaccess/nanumgothic.css");
 //@import '/src/assets/styles/less/variables';  // 이것도 가능
 //@import '/src/assets/styles/less/mixins';     // 이것도 가능
-@import 'stylesPath/less/variables'; // ~ 없이도 정상 동작
+@import 'stylesPath/less/variables'; // ~ 없이도 정상 동작. css 파일 import 해 보니 없으니 정상동작 하지 않는다. 있어야 하는듯..근데 여긴 없어도 되네???
 @import '~stylesPath/less/mixins';
 // less 는 @ 를 붙여 변수를 선언하고
 // sass 는 $ 를 붙여 변수를 선언한다.
