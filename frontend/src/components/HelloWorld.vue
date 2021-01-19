40e9281426a0ba905745fbe1b403da3f78f012e0
DanTe <cldroddl@naver.com>
2021-01-20 03:11:47 +0900

    vue 파일의 template 에서 static 리소스 사용하는 방법

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -2,15 +2,21 @@
   <div class="container">
     <h1>Bootstrap starter template</h1>
     <p class="lead">
-      <a>Use this document</a> as a way to quickly start any new project.<br />
+      <a>Use this document</a> as a way to quickly start any new project.<br/>
       All you get is this text and a mostly barebones HTML document.
     </p>
     <div class="row">
-      <div class="card" style="width: 18rem;">
-        <img src="" class="card-img-top" alt="Card Image">
+      <div class="card" style="width: 18rem; text-anchor: middle;">
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg"
+             preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap">
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#868e96"></rect>
+          <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+        </svg>
         <div class="card-body">
           <h5 class="card-title">About</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
           <a href="/about" class="btn btn-primary">about</a>
         </div>
       </div>
@@ -18,7 +24,8 @@
         <img src="" class="card-img-top" alt="Card Image">
         <div class="card-body">
           <h5 class="card-title">flex</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
           <a href="/flex" class="btn btn-primary">flex</a>
         </div>
       </div>
@@ -26,8 +33,19 @@
         <img src="" class="card-img-top" alt="Card Image">
         <div class="card-body">
           <h5 class="card-title">LESS Menu</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
           <a href="/lessmenu" class="btn btn-primary">LESS Menu</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <!-- template 에서는 오히려 ~@ 이것은 먹히질 않고 @ 나 ../assets/ 로 해야 정상 동작하네 -->
+        <img src="@/assets/images/p_images/slider_01.jpg" class="card-img-top" alt="Card Image">
+        <div class="card-body">
+          <h5 class="card-title">Flex2</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <a href="/flex2" class="btn btn-primary">Flex2</a>
         </div>
       </div>
     </div>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home container">
+    <!-- 여기서는 이미지 경로를 프로젝트 상대 경로로 접근해도 되네??? -->
+    <!-- 이렇게 assets 경로에 접근해도 컴파일 후 img 폴더로 변환되고 리소스도 복사된다. -->
+    <!-- 그런데 css 에서는 ~@/assets 로 접근해야만 되든데... -->
     <img class="mx-auto d-block" alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
