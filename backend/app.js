5e7f1a75359626728405b0040c6feaa69dc30953
DanTe <cldroddl@naver.com>
2021-02-17 18:57:51 +0900

    res.send 와 res.render 의 차이 코멘트

--- backend/app.js
+++ backend/app.js
@@ -47,6 +47,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  // res.render 는 error.pug 파일을 html로 생성한다.
   res.render('error');
 });
 
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+/*
+ * res.send 는 응답을 json이나 문자열로 보내는 것인고
+ * res.render 는 pug 페이지를 그리는 것이다.
+ */
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('api response');
