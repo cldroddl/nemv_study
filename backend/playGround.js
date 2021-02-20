6f686f19e7ff29c7b808bf60bd9c599740b9bee4
DanTe <cldroddl@naver.com>
2021-02-21 01:03:56 +0900

    성공 여부를 반환하는 updateOne 대신 findOneAndUpdate 로 객체를 반환하여 확인용으로 사용

--- backend/playGround.js
+++ backend/playGround.js
@@ -12,7 +12,6 @@ exports.test = {
   model: () => {
     console.log('모델 테스트')
 
-
     const cp = new Company({
       name: '테스트'
     })
@@ -34,7 +33,9 @@ exports.test = {
     // Group 은 꼭 company_id 가 필요하므로 검색 후 사용한다.
     Company.findOne({name: '테스트'})
       .then(company => {
-        console.log(company._id)
+        // 조회값이 없을수도 있으므로 예외처리. promise chain 에서 나갈 때는 꼭 throw 로 보내야 한다.
+        // return 할 경우 다음 then 으로 간다.
+        if (!company) throw new Error('회사가 존재하지 않음'); // Exception 객체는 없네
         const group = new Group({
           name: '소속1',
           company_id: company._id
@@ -44,7 +45,15 @@ exports.test = {
       .then(group => {  // ~~~ 그룹이 넘어온다. 순차적으로 실행되므로
         console.log(group)
         // array 에 추가는 $addToSet, 값 변경은 $set
-        return Company.updateOne({_id: group.company_id}, {$addToSet: { group_ids: group._id}})
+        // return Company.updateOne({_id: group.company_id}, {$addToSet: { group_ids: group._id}})
+        // updateOne 은 성공여부만 알려주므로 객체를 반환하여 확인용으로 사용하도록 수정
+        // findOneAndUpdate 와 옵션 { new: true } 로 갱신된 도큐먼트를 출력
+        return Company.findOneAndUpdate({
+          _id: group.company_id
+        }, {
+          $addToSet: { group_ids: group._id }
+        }, { new: true
+        }).populate('group_ids')
       })
       .then(result => {
         console.log(result)
